9f91d6e253e250cefe74026aca7b06548415bfa8
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-11-14 11:59:21 +0100

    lab1

---
+++ Mvc-Lab1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_Lab1.Startup))]
+namespace Mvc_Lab1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
