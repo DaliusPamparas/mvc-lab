0266cb9c6b7f571c5cc11f4504009ac6026e11d1
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-11-16 16:59:23 +0100

    created upload page

--- MVC-Lab1-1/Controllers/HomeController.cs
+++ MVC-Lab1-1/Controllers/HomeController.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MVC_test1.Controllers
+namespace MVC_Lab1_1.Controllers
 {
     public class HomeController : Controller
     {
-       
+        // GET: Home
         public ViewResult Index()
+        {
+            ViewBag.ArticleName = "Gallery";
+            return View();
+        }
+        public ViewResult Addphoto()
         {
             return View();
         }
---
+++ MVC-Lab1-1/Models/photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Lab1_1.Models
+{
+    public class photo
+    {
+       public Guid ID { get; set; }
+       public string Name { get; set; }
+       public string Description { get; set; }
+       public string Address { get; set; }
+    }
+}
