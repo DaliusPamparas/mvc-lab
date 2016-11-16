cb51f47fdf5474804f74b12119fa1f829f67f98a
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-11-16 13:25:56 +0100

    Recreating lab1

---
+++ MVC-test1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvc-movie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_movie.Startup))]
+namespace mvc_movie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
