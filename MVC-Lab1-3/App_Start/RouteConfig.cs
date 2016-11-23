a694220ae676acf703b18c39dd97ecff8e7ef93f
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-11-23 19:27:30 +0100

    Admin Area done

--- MVC-Lab1-3/App_Start/RouteConfig.cs
+++ MVC-Lab1-3/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace MVC_Lab1_3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+
+                namespaces:new[] { "MVC_Lab1_3.Controllers" }
             );
         }
     }
